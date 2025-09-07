555f4141620f35d624c74e1276ca56666283ca42
chebupell <kremenvov11@gmail.com>
2025-09-07 22:04:30 +0300

    feat:end

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.min = 0;
+    this.max = 0;
+    this.candidate = 0;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-  lower() { }
+  guess() {
+    this.candidate = Math.round((this.min + this.max) / 2);
+    return this.candidate;
+  }
 
-  greater() { }
-*/
+  lower() {
+    this.max = this.candidate;
+  }
+
+  greater() {
+    this.min = this.candidate;
+  }
 }
 
 module.exports = GuessingGame;
